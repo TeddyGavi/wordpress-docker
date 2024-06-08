3c13f64833aa0b912730d4948b2fc1e333ea3e65
TeddyGavi <matc.davis@gmail.com>
2024-06-07 22:34:27 -0700

    updates

--- phpmyadmin/config/config.secret.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* Encryption key */
-$cfg['blowfish_secret'] = getenv('ENCRYPTION_KEY') ?: 'default_encryption_key';
-
-/* Server parameters */
-$cfg['Servers'][$i]['host'] = 'database';
-$cfg['Servers'][$i]['port'] = '3306';
-$cfg['Servers'][$i]['user'] = 'user';
-$cfg['Servers'][$i]['password'] = '${DATABASE_PASSWORD}';
