5e3bfcef9065dcf8c4e612ff3c6adc06d31b334f
TeddyGavi <matc.davis@gmail.com>
2024-06-09 22:12:21 -0700

    working phpmyadmin

--- phpmyadmin/config/config.inc.php
+++ phpmyadmin/config/config.inc.php
@@ -1,12 +1,12 @@
 <?php
 // Database connection settings
-$cfg['Servers'][$i]['host'] = 'database';  // Docker service name
-$cfg['Servers'][$i]['port'] = '3306';
-$cfg['Servers'][$i]['user'] = 'root';      // MySQL root user
-$cfg['Servers'][$i]['password'] = '${DATABASE_ROOT_PASSWORD}';  // Password from environment variable
+$cfg['Servers'][1]['host'] = getenv('PMA_HOST');
+$cfg['Servers'][1]['port'] = getenv('PMA_PORT');
+$cfg['Servers'][1]['user'] = 'root';
+$cfg['Servers'][1]['password'] = getenv('MYSQL_ROOT_PASSWORD');
 
-$cfg['Servers'][$i]['auth_type'] = 'cookie';  // Can be 'cookie', 'config', 'http', 'signon'
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][1]['auth_type'] = 'cookie';  // Can be 'cookie', 'config', 'http', 'signon'
+$cfg['Servers'][1]['AllowNoPassword'] = false;
 
 // Security settings
 // $cfg['LoginCookieValidity'] = 3600;  // Cookie validity time in seconds
